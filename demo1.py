72ae800e36e8eb874fc9d3dfc3ddb7e9cbdf83e5
Edham Arief Dawillah / edthix <edthix@gmail.com>
2021-12-06 22:30:48 +0800

    update demo1

--- demo1.py
+++ demo1.py
@@ -1,41 +1,77 @@
 #!/usr/bin/python
 
-import requests
-import json
-import random
-from time import sleep
-import telegram
+"""
+ref1 : https://learn.adafruit.com/dht-humidity-sensing-on-raspberry-pi-with-gdocs-logging/python-setup
+
+1. Get DHT on GPIO4
+2. Send to favoriot
+3. If temperature > MAX_TEMP send to telegram
+"""
 
 from requests.structures import CaseInsensitiveDict
+import adafruit_dht
+import board
+import json
+import random
+import requests
+import telegram
+import time
 
+
+# ********************************************************************************
+# Configurations change here
+# ********************************************************************************
 api_url = "https://apiv2.favoriot.com/v2/streams"
-device_developer_id = ""
-api_key = ""
+device_developer_id = "GET FROM FAVORIOT"
+api_key = "GET FROM FAVORIOT"
 
+bot_secret_key = "GET FROM TELEGRAM"
+telegram_channel_id = "GET FROM TELEGRAM"
+telegram_text = "PANAS!!!"
+
+max_temp = 24 # change accordingly
+
+sleep_time = 2 # number of seconds to sleep before processing
+# ********************************************************************************
+# End of configurations
+# ********************************************************************************
+
+
+# initiate our telegram bot
+bot = telegram.Bot(token=bot_secret_key)
+
+# initiate our dht22 sensor
+dht22 = adafruit_dht.DHT22(board.D4) # D4 = GPIO4
+
+# Setup request headers
 headers = CaseInsensitiveDict()
 headers["Content-Type"] = "application/json"
 headers["apiKey"] = api_key
 
+def main():
+    """
+    Main function
+    """
+    while True:
+        time.sleep(sleep_time)
+        temp_c = dht22.temperature
+        print("Temperature is", temp_c)
 
-bot_secret_key = ""
-telegram_channel_id = ""
-bot = telegram.Bot(token=bot_secret_key)
+        favoriot_payload = json.dumps({
+            "device_developer_id": device_developer_id,
+            "data": {
+                "temperature": temp
+            }
+        })
+
+        res = requests.post(api_url, headers=headers, data=payload)
+        print(res)
+
+        if temp > max_temp:
+            bot.send_message(chat_id=telegram_channel_id, text=telegram_text)
+
+        print("--------------------------------------------------------------------------------")
 
 
-for i in range(20):
-    sleep(5)
-    temp = random.randrange(-2, 35)
-    print(temp)
-
-    payload = json.dumps({
-    "device_developer_id": device_developer_id,
-    "data": {
-        "temperature": temp
-    }
-    })
-
-    r = requests.post(api_url, headers=headers, data=payload)
-    print(r)
-
-    if temp > 37:
-        bot.send_message(chat_id=telegram_channel_id, text="Notification IOT - check me I'm hot")
+if __name__ == "__main__":
+    main()
