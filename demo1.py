e8b52dea66aef6db0e677111667921c9e0bdc3fd
Edham Arief Dawillah / edthix <edthix@gmail.com>
2021-12-07 17:17:53 +0800

    handle exception in demo1

--- demo1.py
+++ demo1.py
@@ -22,11 +22,11 @@ import RPi.GPIO as GPIO
 # Configurations change here
 # ********************************************************************************
 api_url = "https://apiv2.favoriot.com/v2/streams"
-device_developer_id = "GET FROM FAVORIOT"
-api_key = "GET FROM FAVORIOT"
+device_developer_id = "deviceDefault@ruzainah"
+api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJ1emFpbmFoIiwicmVhZF93cml0ZSI6dHJ1ZSwiaWF0IjoxNjM3OTEzNzk4fQ.5X1wPDtf663_QEFh13gPeITquaJmK0xioi5eTX2Jtw0"
 
-bot_secret_key = "GET FROM TELEGRAM"
-telegram_channel_id = "GET FROM TELEGRAM"
+bot_secret_key = "2105205678:AAEsYZm13kP9asseyAZBT4CgqjCjXXVW8ZI"
+telegram_channel_id = "-1001722314638"
 telegram_text = "PANAS!!!"
 
 max_temp = 24  # change accordingly to send to telegram
@@ -55,6 +55,18 @@ headers["Content-Type"] = "application/json"
 headers["apiKey"] = api_key
 
 
+def favoriot_payload(device_developer_id, data):
+    """
+    Generate favoriot payload
+    """
+    return json.dumps(
+        {
+            "device_developer_id": device_developer_id,
+            "data": data,
+        }
+    )
+
+
 def soil_moisture_callback(channel):
     """
     Callback for soil_moisture sensor
@@ -68,17 +80,14 @@ def soil_moisture_callback(channel):
         print("Water detected")
         soil_moisture_reading = 1
 
-    payload = json.dumps(
-        {
-            "device_developer_id": device_developer_id,
-            "data": {"soil_moisture": soil_moisture_reading},
-        }
+    payload = favoriot_payload(
+        device_developer_id, {"soil_moisture": soil_moisture_reading}
     )
-    request.post(api_url, headers=headers, data=payload)
+    requests.post(api_url, headers=headers, data=payload)
 
 
 GPIO.add_event_detect(
-    soil_moisture_channel, GPIO.BOTH, bouncetime=1000
+    soil_moisture_channel, GPIO.BOTH, bouncetime=5000
 )  # let us know when the pin goes HIGH or LOW
 GPIO.add_event_callback(
     soil_moisture_channel, soil_moisture_callback
@@ -94,26 +103,26 @@ def main():
             sleep_time
         )  # we want to sleep for few seconds to let the sensors capture data
 
-        temp_c = dht22.temperature  # in celcius
-        print("Temperature is", temp_c)
+        try:
+            temp_c = dht22.temperature  # in celcius
+            print("Temperature is", temp_c)
 
-        # What we want to send to favoriot
-        payload = json.dumps(
-            {
-                "device_developer_id": device_developer_id,
-                "data": {"temperature": temp_c},
-            }
-        )
+            # What we want to send to favoriot
+            payload = favoriot_payload(device_developer_id, {"temperature": temp_c})
 
-        res = requests.post(api_url, headers=headers, data=payload)
-        print(res)
+            res = requests.post(api_url, headers=headers, data=payload)
+            print(res)
 
-        if temp_c > max_temp:
-            bot.send_message(chat_id=telegram_channel_id, text=telegram_text)
+            if temp_c > max_temp:
+                bot.send_message(chat_id=telegram_channel_id, text=telegram_text)
 
-        print(
-            "--------------------------------------------------------------------------------"
-        )
+            print(
+                "--------------------------------------------------------------------------------"
+            )
+        except:
+            print(
+                "error detected during reading of DHT22 sensor - do nothing and wait for next reading"
+            )
 
 
 if __name__ == "__main__":
